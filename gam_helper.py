71f48e7eef7df86ae4ab43e23620f2af3e5bfc18
GAM-Oli <stefano.marzolla@olitaliagroup.com>
2025-08-01 14:15:56 +0200

    Update gam_helper.py

--- gam_helper.py
+++ gam_helper.py
@@ -19,7 +19,7 @@ if query:
         context = get_wiki_text()
 
         response = client.chat.completions.create(
-            model="gpt-4",
+            model="gpt-3.5-turbo",
             messages=[
                 {
                     "role": "system",
